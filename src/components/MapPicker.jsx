42281d11e8bc10c50109278ddea11d847c50092f
Dev Wahi <devwahi@Devs-MacBook-Air.local>
2025-07-05 23:22:28 +0530

    Update: Switched map

--- src/components/MapPicker.jsx
+++ src/components/MapPicker.jsx
@@ -1,9 +1,10 @@
 // src/components/MapPicker.jsx
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMapEvents, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
+// 📌 Fix for missing marker icons in Vite/Webpack
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
@@ -17,7 +18,7 @@ function LocationMarker({ setCoords }) {
   useMapEvents({
     click(e) {
       setPosition(e.latlng);
-      setCoords(e.latlng); // update parent
+      setCoords(e.latlng); // Update parent state
     }
   });
 
@@ -25,11 +26,31 @@ function LocationMarker({ setCoords }) {
 }
 
 export default function MapPicker({ setCoords }) {
+  const [indiaGeoJson, setIndiaGeoJson] = useState(null);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/geohacker/india/master/state/india_states.geojson')
+      .then(res => res.json())
+      .then(data => setIndiaGeoJson(data))
+      .catch(console.error);
+  }, []);
+
   return (
-    <MapContainer center={[12.9716, 77.5946]} zoom={12} style={{ height: "300px" }}>
+    <MapContainer center={[22.5937, 78.9629]} zoom={5} style={{ height: "300px", width: "100%" }}>
+      {/* 🌐 Carto Voyager basemap */}
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>, &copy; <a href="https://carto.com/">CARTO</a>'
       />
+
+      {/* 🇮🇳 Optional India boundary overlay */}
+      {indiaGeoJson && (
+        <GeoJSON
+          data={indiaGeoJson}
+          style={{ color: 'black', weight: 1, fillOpacity: 0.03 }}
+        />
+      )}
+
       <LocationMarker setCoords={setCoords} />
     </MapContainer>
   );
